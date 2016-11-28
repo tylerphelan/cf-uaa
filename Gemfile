30704c1b93099f8b31efd585c49dde72a25f189d
Tyler Phelan <tphelan@pivotal.io>
2016-11-28 16:11:49 -0500

    Set up Rspec
    Signed-off-by: Yuki Nishijima <ynishijima@pivotal.io>

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in cf-uaa.gemspec
 gemspec
+
+gem 'byebug'
+gem 'pry'
+gem 'pry-byebug'
+gem 'http_logger'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "cf/uaa"
+require "logger"
+
+require 'bundler'
+Bundler.require
+
+HttpLogger.logger =  Logger.new('tmp/http.log')
+HttpLogger.colorize = true
+HttpLogger.log_headers = true
